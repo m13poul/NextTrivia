2c0f2b37619718bb2513f15f389cf5288bf44045
M2013030 <christosp1990@gmail.com>
2022-04-06 15:07:40 +0200

    NextAuth GoogleProvider added

--- components/Convincer.js
+++ components/Convincer.js
@@ -1,5 +1,8 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
+import { SiGithub } from "react-icons/si";
+import { FcGoogle } from "react-icons/fc";
+import { IconContext } from "react-icons";
 
 const Convincer = () => {
   const { data: session, status } = useSession();
@@ -8,7 +11,7 @@ const Convincer = () => {
     <div className="text-2xl">
       <div className=" ">
         Setup the settings and playing immediately! <br />
-        You can also Login via your GitHub account to enjoy more features like:
+        You can also Login via your GitHub <br /> or Google account to enjoy more features like:
         <ul>
           <li>Save all your past games and your best score</li>
           <li>The time and date you played each game</li>
@@ -18,19 +21,36 @@ const Convincer = () => {
       </div>
       {session ? (
         <Link href="/profile" passHref>
-          <button
-            className="text-lg mt-8 bg-orange-500 p-2 rounded-md"
-          >
+          <button className="text-lg mt-8 bg-orange-500 p-2 rounded-md">
             My Profile
           </button>
         </Link>
       ) : (
-        <button
-          className="text-lg mt-8 bg-orange-500 p-2 rounded-md"
-          onClick={() => signIn("github")}
-        >
-          Sign In with <span className="text-gray-700">GitHub</span>!
-        </button>
+        <div>
+          <button
+            className="text-lg mt-8 bg-orange-500 p-2 rounded-md flex items-center gap-x-1"
+            onClick={() => signIn("github")}
+          >
+                        <IconContext.Provider value={{ style: { fontSize: "30px" } }}>
+              <SiGithub />
+            </IconContext.Provider>
+            <p>
+              Sign In with <span className="text-gray-700">GitHub</span>!
+            </p>
+          </button>
+          <br />
+          <button
+            className="text-lg mt-8 bg-[#4285F4] p-2 rounded-md flex items-center gap-x-1"
+            onClick={() => signIn("google")}
+          >
+            <IconContext.Provider value={{ style: { fontSize: "30px" } }}>
+              <FcGoogle />
+            </IconContext.Provider>
+            <p>
+              Sign In with <span className="">Google</span>!
+            </p>
+          </button>
+        </div>
       )}
     </div>
   );
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import MongoClientPromise from '../../../lib/mongodb'
 
@@ -20,6 +21,17 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     })
     // ...add more providers here
   ],
